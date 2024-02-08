4bd5d6cf51fc82d9d652729fa7bf60fa1a09e1b2
ma0106 <158456502+ma0106@users.noreply.github.com>
2024-02-08 10:18:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,150 +1,46 @@
-let noButtonClickCount = 0; // Contador para el botón "No"
-let noButtonState = 0; // Estado actual del botón "No"
+$(document).ready(function () {
+  var magic8Ball = {};
+  //Lista de respuestas posibles
+  magic8Ball.listOfAnswers = [
+    "No",
+    "Si",
+    "No creo la verdad...",
+    "¿Cuándo?",
+    "Murcia no existe",
+    "xD",
+  ];
+  $("#answer").hide();
 
-// Mostrar el gif inicial
-document.getElementById("gifContainer").style.display = "block";
+  var onClick = function () {
+    //Para el dialogo de la respuesta
+    $("#answer").hide();
+    $("#8ball").attr(
+      "src",
+      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png"
+    );
+    setTimeout(function () {
+      //Para la ventana emergente de si o no
+      var question = prompt("HAZ UNA PREGUNTA DE RESPUESTA SI O NO");
+      magic8Ball.askQuestion(question);
+    }, 500);
+  }; //Fin funcion onClick
 
+  magic8Ball.askQuestion = function (question) {
+    $("#8ball").effect("shake");
+    $("#8ball").attr(
+      "src",
+      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png"
+    );
+    $("#answer").fadeIn(4000);
+    var randomNumber = Math.random(); //Generamos numero aleatorio
+    //Seleccionamos una respuesta aleatoria de la lista de respuestas
+    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumberForListOfAnswers);
+    var answer = this.listOfAnswers[randomIndex];
+    $("#answer").text(answer);
+    console.log(question);
+    console.log(answer);
+  }; //Fin funcion preguntar
 
-//Funcion que se activa cuando le damos al boton Si
-document.getElementById("siBtn").addEventListener("click", function () {
-  // Ocultar el gif triste y mostrar el gif feliz
-  document.getElementById("sadGifContainer").style.display = "none";
-  document.getElementById("sadGifContainer1").style.display = "none";
-  document.getElementById("sadGifContainer2").style.display = "none";
-  document.getElementById("gifContainer").style.display = "none";
-  document.getElementById("happyGifContainer").style.display = "block";
-
-  // Ocultar los botones "Pregunta Sí" y "No "
-  document.getElementById("question").style.display = "none";
-  document.getElementById("siBtn").style.display = "none";
-  document.body.classList.add("bg-green");
-  document.getElementById("noBtn").style.display = "none";
-
-  // Mostrar el mensaje específico
-  document.getElementById("messageContainer").style.display = "block";
-  document.getElementById("messageContainer").innerHTML = "😊😍";
-
-  // Mostrar otro gif después de 3 segundos
-  setTimeout(function () {
-    document.getElementById("happyGifContainer").style.display = "none";
-    document.getElementById("happyGifContainer2").style.display = "block";
-  }, 1000);
-
-  setTimeout(function () {
-    document.getElementById("happyGifContainer2").style.display = "none";
-    document.getElementById("happyGifContainer3").style.display = "block";
-  }, 2000);
-  setTimeout(function () {
-    document.getElementById("happyGifContainer3").style.display = "none";
-    document.getElementById("happyGifContainer4").style.display = "block";
-  }, 3000);
-});
-
-document.getElementById("noBtn").addEventListener("click", function () {
-  switch (noButtonState) {
-    case 0:
-      // Primera vez haciendo clic en "No"
-      document.getElementById("happyGifContainer").style.display = "none";
-      document.getElementById("gifContainer").style.display = "none";
-      document.getElementById("sadGifContainer").style.display = "block";
-
-      // Modificar el botón "No"
-      document.getElementById("noBtn").innerHTML = "¿PORQUE NO ME QUIERES?😭";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-
-      document.getElementById("siBtn").style.fontSize = "50px";
-      document.getElementById("siBtn").style.padding = "30px 50px";
-      document.getElementById("siBtn").innerHTML = "DADLE A QUE SI😭"
-
-      noButtonClickCount++;
-      noButtonState++;
-      break;
-
-    case 1:
-      // Segunda vez haciendo clic en "No"
-
-      document.getElementById("noBtn").innerHTML = "NOOOOO";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-      document.getElementById("sadGifContainer").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "block";
-
-      // Hacer que el botón "Sí" crezca
-      document.getElementById("siBtn").style.fontSize = "30px";
-      document.getElementById("siBtn").style.padding = "30px 50px";
-      document.getElementById("siBtn").innerHTML = "QUE SIIIIII"
-
-      noButtonState++;
-      break;
-
-    case 2:
-      document.getElementById("noBtn").innerHTML ="NO LE DES AL ROJOOOOO";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-      document.getElementById("sadGifContainer").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "none";
-      document.getElementById("sadGifContainer1").style.display = "block";
-
-      // Hacer que el botón "Sí" crezca
-      document.getElementById("siBtn").style.fontSize = "30px";
-      document.getElementById("siBtn").style.padding = "30px 50px";
-      document.getElementById("siBtn").innerHTML = "SII QUIERES😭"
-
-      noButtonState++;
-      break;
-
-    case 3:
-      document.getElementById("noBtn").innerHTML = "¿PORQUE LE DAS AL ROJO?😭";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-      document.getElementById("sadGifContainer").style.display = "none";
-      document.getElementById("sadGifContainer1").style.display = "none";
-      document.getElementById("sadGifContainer3").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "block";
-
-      // Hacer que el botón "Sí" crezca
-      document.getElementById("siBtn").style.fontSize = "30px";
-      document.getElementById("siBtn").style.padding = "30px 50px";
-      document.getElementById("siBtn").innerHTML = "DADLE AL VERDE😔"
-
-      noButtonState++;
-      break;
-    case 4:
-      document.getElementById("noBtn").innerHTML = "ME VAS A MATAR SI LE DAS AL ROJO";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-      document.getElementById("sadGifContainer").style.display = "none";
-      document.getElementById("sadGifContainer1").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "none";
-      document.getElementById("sadGifContainer3").style.display = "block";
-
-      // Hacer que el botón "Sí" crezca
-      document.getElementById("siBtn").style.fontSize = "30px";
-      document.getElementById("siBtn").style.padding = "30px 50px";
-      document.getElementById("siBtn").innerHTML = "😔YA VEO😔"
-
-      noButtonState++;
-      break;
-    case 5:
-      document.getElementById("noBtn").innerHTML = "ME HAS MATADO";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-      document.getElementById("sadGifContainer1").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "none";
-      document.getElementById("sadGifContainer3").style.display = "none";
-      document.getElementById("sadGifContainer").style.display = "block";
-
-
-    case 6:
-      document.getElementById("noBtn").innerHTML = "por favooooooor";
-      document.getElementById("noBtn").style.backgroundColor = "#F1330A";
-
-      document.getElementById("sadGifContainer").style.display = "none";
-      document.getElementById("sadGifContainer1").style.display = "none";
-      document.getElementById("sadGifContainer2").style.display = "none";
-      document.getElementById("gifContainer").style.display = "block";
-      document.getElementById("happyGifContainer").style.display = "none";
-      // Vuelve al estado 0
-      noButtonState = 0;
-      break;
-
-    default:
-      // Por si acaso, maneja cualquier otro caso aquí
-      break;
-  }
+  $("#questionButton").click(onClick); //Cuando le damos al boton de preguntas
 });
